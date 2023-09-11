d4de32feefecc57595b704f2950da54fb06fe878
kina12123434 <af20090@shibaura-it.ac.jp>
2023-09-11 13:53:45 +0900

    FMtest

--- FM/FMtest-main.cxx
+++ FM/FMtest-main.cxx
@@ -1,134 +1,80 @@
-#include"../src/tensor.h"
-#include"../src/vector3d.h"
+#include"../src/matrix.h"
+#include"../src/matrix.h"
 
 int main(){
+      int K = 3;
+      Matrix X(25, 10,0.0);
+      Vector Y(X.rows(), 0.0, "all");
+      Matrix V(K,X.cols());
+
+      int line_num;
+      for (int i=0; i < 5; i++){
+      for(int j = 0; j < 5; j++){
+      X[line_num][i] = 1.0;//
+      X[line_num][5+j] = 1.0;//
+      //std::cout << i+1 << " " << i << ":" << X[line_num][i] << " " << 5+j << ":" << X[line_num][5+j] << std::endl;
+      line_num++;
+      }
+    }
+
+     V = {{0.363705,0.787761,0.158121,0.432216,0.268406,0.260643,0.825071,0.595871,0.431434,0.682668},
+      {0.135,0.0665437,0.124959,0.114106,0.57664,0.941527,0.0570628,0.4042,0.527303,0.089849},
+      {0.92168,0.612376,0.979481,0.894522,0.77165,0.213524,0.562141,0.693945,0.731995,0.725184}};
+    //std::cout << "FM v: "<< V << std::endl;
+
+    double error = 0.0;
+    double err = 0.0;\
+    double omomi = 1 / X.cols();
+    Vector w(X.cols(),omomi,"all");
+    bool ParameterNaN = false;
+    Vector sum(K,0,"all");
+    Vector squareSum(K,0,"all");
+    double prediction = 0.0 , linearTerm, w_0 = 1.0;
+    w = {1,1.1,1.5,1.7,1.9,1.1,1.3,1.5,1.7,1.9};
 	
-  Tensor V(2,4,3,1.0);
-  Vector3d Vd(2,4,3,1.0);
-  double count;
-  for(int k=0;k<2;k++){
-    count =1.0;
-    for(int i=0;i<4;i++){
-      for(int j=0;j<3;j++){
-        if(k==0){
-          Vd[k][i][j] = count;
-          V[k](i,j) = count;
-          count++;
-        }else if (k==1){
-          Vd[k][i][j] = count*2;
-          V[k](i,j) = count*2;
-          count++;
+  for(int line = 0; line < X.rows(); line++){
+        //for(int j = 0; j < SparseIncompleteData[i].essencialSize(); j++){
+            prediction = 0.0;
+            linearTerm = 0.0;
+            // 線形項の計算
+            for (int i = 0; i < X.cols(); i++) {
+                linearTerm += w[i] * X[line][i];
+            }
+            //std::cout << "FM: linearTerm end : " << step << std::endl;
+
+            // 交互作用項の計算
+            for (int factor = 0; factor < K; factor++) {
+                sum[factor] = 0.0;
+                squareSum[factor] = 0.0;
+                for (int i = 0; i < X.cols(); i++) {
+                    sum[factor] += V[factor][i] * X[line][i];
+                    squareSum[factor] += V[factor][i] * V[factor][i] * X[line][i] * X[line][i];
+                }
+                //interactionTerms[factor] = 0.5 * (sum * sum - squareSum);
+                prediction += 0.5 * (sum[factor] * sum[factor] - squareSum[factor]);
+            }
+            //std::cout << "FM: prediction end : " << step << std::endl;
+
+            // 予測値と誤差の計算
+            prediction += linearTerm;
+            prediction += w_0;
+            Y[line] = prediction;
+  }
+  //std::cout << "FM: prediction end : " << Y << std::endl;
+  //sparseにファイル出力
+  int userNum=5, itemNum=5;
+  int count=0;
+  int line = 0;
+
+
+    for(int i=0;i<userNum;i++){
+      std::cout << itemNum << " ";
+        for(int k=0;k<itemNum;k++){
+            std::cout << k << " " << Y[line] << " " ;
+            line++;
         }
-      }
+      std::cout << std::endl;
     }
-  }
-  Tensor U(2,5,3,1.0);
-  Vector3d Ud(2,5,3,1.0);
-  for(int k=0;k<2;k++){
-    count =1.0;
-    for(int i=0;i<5;i++){
-      for(int j=0;j<3;j++){
-        if(k==0){
-          Ud[k][i][j] = count;
-          U[k](i,j) = count;
-          count++;
-        }else if (k==1){
-          Ud[k][i][j] = count*2;
-          U[k](i,j) = count*2;
-          count++;
-        }
-      }
-    }
-  }
 
-  Tensor C(2,5,4,1.0);
-  Vector3d Cd(2,5,4,1.0);
-  for(int k=0;k<2;k++){
-    count =1.0;
-    for(int i=0;i<5;i++){
-      for(int j=0;j<4;j++){
-        if(k==0){
-          Cd[k][i][j] = count;
-          C[k](i,j) = count;
-          count++;
-        }else if (k==1){
-          Cd[k][i][j] = count*2;
-          C[k](i,j) = count*2;
-          count++;
-        }
-      }
-    }
-  }
-      std::cout << "U:\n" << U << std::endl;
-      std::cout << "V:\n" << V << std::endl;
-
-      Cd = all(Cd,0.0);
-      std::cout << "C:\n" << Cd << std::endl;
-
-    Vector count_U(Ud.rows(),1,"all");
-      count_U = all(count_U,0.0);
-      std::cout << "U_c:\n" << count_U << std::endl;
-
-     // std::fill(count_U[0], count_U[Ud.rows()], 0);
-      //std::fill(count_V[0], count_V[Ud.rows()], 0);
-  /*
-  Tensor prev_U(2,5,3, 0.0);
-  Vector3d prev_Ud(2,5,3, 0.0);
-  Vector3d prev_Vd(2,4,3, 0.0);
-  double err,miss=0.0;
-  for(int k=0;k<1;k++){
-      for(int i=0;i<5;i++){
-        for(int j=0;j<4;j++){
-          err = U[k][i]*U[k][i];
-          std::cout << "err:" << err << std::endl;
-          miss = 0.0;
-          for(int d=0;d<3;d++){
-          miss += U[k][i][d]*U[k][i][d];
-          
-          prev_U[k](i,d)+=tensor_mul(V,V,U,k,d,i,j);
-          prev_Ud[k][i][d]+=vector3d_mul(Vd,Vd,Ud,k,d,i,j);
-          prev_Vd[k][j][d]+=vector3d_mul(Ud,Ud,Vd,k,d,j,i);
-          }
-          std::cout << "miss:" << miss << std::endl;
-        }
-      }
-      std::cout << "prev_Ud.cols():\n" << prev_Ud.cols() << std::endl;
-      std::cout << "prev_U:\n" << prev_U[k] << std::endl;
-      std::cout << "prev_Ud:\n" << prev_Ud[k] << std::endl;
-      std::cout << "prev_Ud:\n" << prev_Vd[k] << std::endl;
-  }
-  
-  std::cout << "--------------------------------------------------------------------------" << std::endl;
-
-  Tensor prev_U_CV(2,5,3, 0.0);
-  Vector3d prev_U_CV_d(2,5,3, 0.0);
-  for(int k=0;k<1;k++){
-      for(int i=0;i<5;i++){
-        for(int j=0;j<4;j++){
-          for(int d=0;d<3;d++){
-          prev_U_CV[k](i,d) += C[k](i,j)*V[k](j,d);
-          prev_U_CV_d[k][i][d] += Cd[k][i][j]*Vd[k][j][d];
-          }
-        }
-      }
-      std::cout << "prev_U:\n" << prev_U_CV[k] << std::endl;
-      std::cout << "prev_U:\n" << prev_U_CV_d[k] << std::endl;
-  }
-
-  Tensor prev_V_UC(2,4,3, 0.0);
-  Vector3d prev_V_UC_d(2,4,3, 0.0);
-  for(int k=0;k<1;k++){
-      for(int i=0;i<5;i++){
-        for(int j=0;j<4;j++){
-          for(int d=0;d<3;d++){
-          prev_V_UC[k](j,d) += U[k](i,d)*C[k](i,j);
-          prev_V_UC_d[k][j][d] += Ud[k][i][d]*Cd[k][i][j];
-          }
-        }
-      }
-      std::cout << "prev_V:\n" << prev_V_UC[k] << std::endl;
-      std::cout << "prev_V:\n" << prev_V_UC_d[k] << std::endl;
-  }
-  */
   return 0;
 }
